bffc9070a3645df1bd5c2cd2299fcd6f3ad0e28b
HySteffensen <hyrum.steffensen@gmail.com>
2018-05-03 15:00:56 -0700

    added about.html and an api route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import config from './config';
+import apiRouter from './api';
 
 import express from 'express';
 const server = express();
@@ -7,10 +8,9 @@ server.get('/', (req, res) => {
   res.send('Hello Express');
 });
 
-server.get('/about.html', (req, res) => {
-  res.send('The about page');
-});
+server.use('/api', apiRouter);
+server.use(express.static('public'));
 
 server.listen(config.port, () =>{
-  console.info('Express listeningon port ', config.port);
+  console.info('Express listening on port ', config.port);
 });
