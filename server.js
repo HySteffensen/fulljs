70ef1f9c85740c0f26c710c6eb4679abdc92edfa
HySteffensen <hyrum.steffensen@gmail.com>
2018-05-04 12:55:07 -0700

    project cleanup

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ server.set('view engine', 'ejs');
 
 server.get('/', (req, res) => {
   res.render('index', {
-    content: 'Hello Express and <em>EJS</em>!'
+    content: '...'
   });
 });
 
 server.use('/api', apiRouter);
 server.use(express.static('public'));
 
-server.listen(config.port, () =>{
-  console.info('Express listening on port ', config.port);
+server.listen(config.port, () => {
+  console.info('Express listening on port', config.port);
 });
