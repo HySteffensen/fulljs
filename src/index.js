c59392594d08a8aa4c06c7a0a747f9b3b86c70f1
HySteffensen <hyrum.steffensen@gmail.com>
2018-05-04 12:19:54 -0700

    added headerMessage prop

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const color = Math.random() > 0.5 ? 'green' : 'red';
+const App = (props) => {
+  return (
+    <h2 className="text-center">
+      {props.headerMessage}
+    </h2>
+  );
+};
+
+App.propTypes = {
+  headerMessage: React.PropTypes.string.isRequired
+};
+
+App.defaultProps = {
+  headerMessage: 'Hello!!'
+};
 
 ReactDOM.render(
-  <h2 style={{color: color}}>
-    Hello React with JSX!! -- {Math.random()}
-  </h2>,
+  <App headerMessage='Hello props!' />,
   document.getElementById('root')
 );
