da02952dc3e22a8d3b4486d0c9e081183ec6ec61
HySteffensen <hyrum.steffensen@gmail.com>
2018-05-05 13:26:46 -0700

    component mount/unmount demonstration

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,14 @@ class App extends React.Component {
   state = {
     pageHeader: 'Naming Contests'
   };
+  componentDidMount() {
+    // timers, listeners
+    console.log('did mount');
+  }
+  componentWillUnmount() {
+    // clean timers, listeners
+    console.log('will Unmount');
+  }
   render() {
     return (
       <div className="App">
--- src/index.js
+++ src/index.js
@@ -7,3 +7,10 @@ ReactDOM.render(
   <App headerMessage='Hello props!' />,
   document.getElementById('root')
 );
+
+setTimeout(() => {
+  ReactDOM.render(
+    <h2>...</h2>,
+    document.getElementById('root')
+  );
+}, 4000);
